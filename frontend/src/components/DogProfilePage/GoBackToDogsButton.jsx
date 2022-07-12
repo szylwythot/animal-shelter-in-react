19ac386700a46638d12744a33de4037fcfbdbaf4
toth_szilvia <szylwy@gmail.com>
2022-07-12 15:37:47 +0200

    fix go to doggies button

--- frontend/src/components/DogProfilePage/GoBackToDogsButton.jsx
+++ frontend/src/components/DogProfilePage/GoBackToDogsButton.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import Button from '@mui/material/Button';
+import { Link } from "react-scroll";
 
 
 function GoBackToDogsButton() {
     return (
-        <Button variant="contained" size="large" className="goBackToDogsBtn" color="secondary" >Back to Dogies</Button>
+        <Link activeClass="active" to="DogFilter" spy={true} smooth={true} duration={1000}>
+            <Button variant="contained" size="large" className="goBackToDogsBtn" color="secondary" >Back to Dogies</Button>
+        </Link>
     )
 };
 
